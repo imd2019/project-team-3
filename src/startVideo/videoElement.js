ea33055242d8e5fc34bcc73da0b0a76c34d3f2a5
larsibarsi <58939896+menjazovutlars@users.noreply.github.com>
2020-07-04 15:36:31 +0200

    branch fix

---
+++ src/startVideo/videoElement.js
@@ -0,0 +1,24 @@
+import DisplayObject from "../displayObject.js";
+
+export default class VideoElement extends DisplayObject {
+  constructor(x, y, width, height, video) {
+    super(x, y, width, height, video);
+    this.volume = 1;
+  };
+
+  draw() {
+    image(this.backgnd, 0, 0, this.width, this.height);
+    this.backgnd.volume(this.volume);
+  }
+
+  play() {
+    this.backgnd.show();
+    this.backgnd.size(this.height);
+    this.backgnd.play();
+  }
+
+  stop() {
+    this.backgnd.stop();
+    this.backgnd.hide();
+  }
+}
